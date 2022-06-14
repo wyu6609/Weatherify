e7de78c57361272494291d62ed673a1765a7da77
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-06-14 18:36:08 -0400

    axios request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import react, { useState, useEffect } from "react";
 import "./App.css";
 import NavbarComponent from "./components/NavbarComponent";
-import "bootstrap/dist/css/bootstrap.min.css";
 
+import Today from "./routes/Today";
+import Weekly from "./routes/Weekly";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+
+const apiKey = "f270ab85b7df432a894221127221406";
+const city = "new york";
+const lat = "40.593081";
+const lon = "-73.974021";
+const zipCode = "11223";
 function App() {
+  //on load fetch weather
+  useEffect(() => {
+    fetchWeather();
+  }, []);
+
+  //fetch weather
+  const fetchWeather = async () => {
+    try {
+      const resp = await axios.get(
+        `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${zipCode}&days=10&aqi=no&alerts=no`
+      );
+      console.log(resp.data);
+    } catch (err) {
+      // Handle Error Here
+      console.error(err);
+    }
+  };
+
   return (
     <div className="App">
       <NavbarComponent />
+      <Routes>
+        <Route path="/" element={<Today />} />
+        <Route path="/weekly" element={<Weekly />} />
+      </Routes>
     </div>
   );
 }
--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -8,11 +8,14 @@ import {
   Form,
   FormControl,
 } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 const NavbarComponent = () => {
+  let navigate = useNavigate();
+
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Brand>Weatherify</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -20,26 +23,25 @@ const NavbarComponent = () => {
             style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
+            <Nav.Link
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Daily{" "}
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/weekly");
+              }}
+            >
+              Next 3 days
             </Nav.Link>
           </Nav>
-          <Form className="d-flex">
+          <Form className="d-flex ">
             <FormControl
               type="search"
-              placeholder="Search"
+              placeholder="Enter zip code or city"
               className="me-2"
               aria-label="Search"
             />
